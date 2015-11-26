490d70df844dcedd66df1bcdda9d64f5dc7a8c2c
Rongsheng Xu <xurongsheng2010@gmail.com>
2015-11-25 23:42:41 -0600

    change MockObject

--- MockObject/MockObject.sh
+++ MockObject/MockObject.sh
@@ -1,5 +1,4 @@
 # !/usr/bash
 
-javac -cp *.jar:. TestTempServlet.java TemperatureServlet.java
-java -cp mockobjects-0.6-core.jar:mockobjects-0.6-j1.4-j2ee1.2.jar:selenium-server-standalone-2.11.0.jar:junit-4.11.jar.org.junit.runner.JUnitCore TestTempServlet
-
+javac -cp *.jar:. TestingLabConverterServletTest.java CityTemperatureServiceProvider.java
+java -cp mockobjects-0.6-core.jar:mockobjects-0.6-j1.4-j2ee1.2.jar:selenium-server-standalone-2.11.0.jar:junit-4.11.jar.org.junit.runner.JUnitCore TestingLabConverterServletTest
